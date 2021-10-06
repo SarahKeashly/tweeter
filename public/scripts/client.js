bb9ce3dd619731c0c27e01d4a7fa25fa7e320602
Sarah <sarahejohnson89@gmail.com>
2021-10-05 20:12:10 -0700

    added counter and made counter turn red when goes into a negative number

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,28 +6,29 @@
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-//takes in a tweet object and returns a tweet article element containing the entire HTML structure of tweet
-const createTweetElement = function() {
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-}
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// }
 
 
 
-const $tweet = createTweetElement(tweetData);
+// const createTweetElement = function() {
+//   const $tweet = $(`<article class="tweet">Hello world</article>`);
+//   return $tweet;
+// }
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+// const $tweet = createTweetElement(tweetData);
+
+// // Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
