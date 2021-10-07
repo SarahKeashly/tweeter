c4102deaa93a59cc8f3689a15195d91f0a974427
Sarah <sarahejohnson89@gmail.com>
2021-10-07 15:40:19 -0700

    created loadtweets function with get request.  Updated createTweet function to be dynamically used without database information.  Adding tweets currently working on browser

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,57 +6,113 @@
 $(document).ready(function() {
   console.log("I am here!")
 
-  // Test / driver code(temporary).Eventually will get this from the server.
-  // const tweetData = {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": "https://i.imgur.com/73hZDYK.png",
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // };
 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+
+  /////////////Post request/////////////
+  //on submit - prevents event from going to another page
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    console.log("Serialize", $(this).serialize());
+
+    //sends form data to the server
+    let url = "/tweets";
+    let serializeData = $(this).serialize();
+
+    //ajax request that posts data to "/tweets"
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: serializeData
+
+    })
+      .then((result) => {
+        console.log(`result: ${result}`)
+        renderTweets(result.data);
+      })
+      .catch((error) => {
+        console.log(`error: ${JSON.stringify(error)}`)
+      });
+  });
 
 
-  /////////////Ask Mentor about this/////////////////
-  const renderTweets = function(data) {
+
+
+  ////////////// Get request///////////////////
+
+  /// kick off ajax in background, hit the url and make request to server.  once we get data back, we are going to pass to our 
+  const loadtweets = function() {
+
+    // const $button = $('tweet-text');
+    // $button.on('submit', function() {
+    //   console.log('Button clicked, performing ajax call...')
+
+    //make a request to "/tweets" and receive the array of tweets as JSON
+    let url = "/tweets";
+    // success = renderTweets();
+
+
+    $.ajax({
+      dataType: "json",
+      url: url,
+      type: "GET",
+      // success: success
+
+    })
+      .then((resultTweets) => {
+        console.log("result", resultTweets)
+        renderTweets(resultTweets);
+
+      })
+      .catch((error) => {
+        console.log(`error: ${(error)}`)
+      });
+
+    // });
+
+  };
+  loadtweets();
+
+
+  const createTweetElement = function(tweet) {
+
+
+    const $articleDom = $(`<article class="article-tweet-container">
+    <header class="article-tweet-header">
+      <div class="article-name">
+      <p> <img class="newton-avatar" src=" ${tweet["user"]["avatars"]}"/>
+      ${tweet["user"]["name"]}
+        </p>
+      </div>
+      <div class="article-username">
+        <p>${tweet["user"]["handle"]}</p>
+      </div>
+    </header>
+    <div class="article-tweet-body">
+      <p>${tweet["content"]["text"]}</p>
+    </div>
+    <hr>
+    <footer class="article-tweet-footer">
+      <p>${timeago.format(tweet["created_at"])}</p>
+      <div class="article-small-images">
+        <div class="flag"><i class="fas fa-flag"></i></div>
+        <div class="retweet"><i class="fas fa-retweet"></i></div>
+        <div class="heart"><i class="fas fa-heart"></i></div>
+      </div>
+      </div>
+    </footer>
+        </article>`)
+
+    return $articleDom;
+  };
+
+
+  //loop through tweet, and for each tweet, I am going to run create tweet element
+  const renderTweets = function(tweets) {
     // loops through tweets
 
-    for (key in data) {
-      // console.log(data[key]);
-      // console.log(data[key]["user"]);
-      // console.log(data[key]["user"]["name"]);
-
-      const $tweetData = createTweetElement(data[key]["user"]);
+    for (const tweet of tweets) {
+      const $tweetData = createTweetElement(tweet);
 
       // takes return value and appends it to the tweets container
       $('#article-tweets-container').append($tweetData);
@@ -86,49 +142,33 @@ $(document).ready(function() {
     ][index];
   };
   timeago.register('pt_BR', locale);
+
+
   // then you can use it
   // timeago.format(1473245023718, 'pt_BR');
 
-  const createTweetElement = function() {
+
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
 
 
-    const $articleDom = $(`<article class="article-tweet-container">
-    <header class="article-tweet-header">
-      <div class="article-name">
-      <p> <img class="newton-avatar" src=" ${data[key]["user"]["avatars"]}"/>
-      ${data[key]["user"]["name"]}
-        </p>
-      </div>
-      <div class="article-username">
-        <p>${data[key]["user"]["handle"]}</p>
-      </div>
-    </header>
-    <div class="article-tweet-body">
-      <p>${data[key]["content"]["text"]}</p>
-    </div>
-    <hr>
-    <footer class="article-tweet-footer">
-      <p>${timeago.format(data[key]["created_at"])}</p>
-      <div class="article-small-images">
-        <div class="flag"><i class="fas fa-flag"></i></div>
-        <div class="retweet"><i class="fas fa-retweet"></i></div>
-        <div class="heart"><i class="fas fa-heart"></i></div>
-      </div>
-      </div>
-    </footer>
-        </article>`)
-
-
-
-    return $articleDom;
-  };
 
 
   // const $tweet = createTweetElement();
-  const $tweet = renderTweets(data);
-
+  // const $tweet = renderTweets(data);
+  // const $tweet = loadtweets(dat);
   // Test / driver code (temporary)
-  // console.log(data.user.name); // to see what it looks like
-  $('#article-tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // console.log("tweet", $tweet); // to see what it looks like
+  // $('#article-tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 });
